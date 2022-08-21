b533aded65203efb473778d45149298502a45dbe
Aadil Adheesh <aadiladheesh@gmail.com>
2022-08-21 22:21:20 +0530

    Committing the chapter: basics

---
+++ swift_programming_language_practice/swift_programming_language_practice/main.swift
@@ -0,0 +1,66 @@
+//
+//  main.swift
+//  swift_programming_language_practice
+//
+//  Created by Aadil Adheesh on 21/08/22.
+//
+
+//import Foundation
+
+//print("Hello, World!")
+
+
+
+//CHAPTER ONE: THE BASICS
+/*
+print("Hello, World!")
+
+var age = 56        //age is of type int and is a variable | This is the 1st way to declare and define a vaurable with an initial state
+var myAge:Int = 44 //This is my 2nd way where data typeis explicitly "annotated" is called type annotation and assigning an intital value
+var newAge:Int    //This is 3rd way, current line is declaration
+newAge = 56      //current line is definition/assignment operation
+print(type(of: age))
+print(type(of: myAge))
+print(type(of: newAge))
+
+print(Int.max)
+print(Int.min)
+
+print(UInt8.max)
+print(UInt8.min)
+
+
+print(Int8.max)
+print(Int8.min)
+print(Int16.max)
+print(Int16.min)
+print(Int32.max)
+print(Int32.min)
+print(Int64.max)
+print(Int64.min)
+
+
+var ht = 175.6
+var height_f:Float = 178.667857647564654
+var height_d:Double
+height_d = 174.54567476453356
+print(height_f)
+print(height_d)
+print(type(of: ht))
+print(type(of: height_f))
+print(type(of: height_d))
+
+print(Double.max)
+print(Float.max)
+
+let capitalOfIndia = "Delhi" //capitalOfIndia is of a type string and is a constant
+print(type(of: capitalOfIndia))
+*/
+
+let three = 3
+let pointdecimals=0.14159
+let pi = Double(three) + pointdecimals
+print(pi)
+
+let integerPi = Int(pi)
+print(integerPi)
