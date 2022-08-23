b3c55f0bb3d1183f13b8c7e6f03e7f5d65973466
Aadil <aadiladheesh@gmail.com>
2022-08-23 13:41:53 +0530

    committing type aliases and booleans in the basics chapter

--- swift_programming_language_practice/swift_programming_language_practice/main.swift
+++ swift_programming_language_practice/swift_programming_language_practice/main.swift
@@ -14,6 +14,10 @@
 //CHAPTER ONE: THE BASICS
 /*
 print("Hello, World!")
+ 
+ 
+//1. Constants and Variables
+  
 
 var age = 56        //age is of type int and is a variable | This is the 1st way to declare and define a vaurable with an initial state
 var myAge:Int = 44 //This is my 2nd way where data typeis explicitly "annotated" is called type annotation and assigning an intital value
@@ -23,6 +27,8 @@ print(type(of: age))
 print(type(of: myAge))
 print(type(of: newAge))
 
+ 
+//2. Integer Bounds
 print(Int.max)
 print(Int.min)
 
@@ -40,6 +46,8 @@ print(Int64.max)
 print(Int64.min)
 
 
+ 
+//3. Floating Point and Double numbers
 var ht = 175.6
 var height_f:Float = 178.667857647564654
 var height_d:Double
@@ -57,10 +65,69 @@ let capitalOfIndia = "Delhi" //capitalOfIndia is of a type string and is a const
 print(type(of: capitalOfIndia))
 */
 
-let three = 3
+//4. Numeric Literals
+/*
+ let decimalnumber = 17
+print(type(of: decimalnumber))
+
+let binarynumber = 0b01001
+print(type(of: binarynumber))
+
+let octalnumber = 0o7272
+print(type(of: octalnumber))
+
+let hexadecimalnumber = 0x73468
+print(type(of: hexadecimalnumber))
+let hexadecimal_double = 0xC.3p0
+print(type(of: hexadecimal_double))
+
+let exponent_double = 1.21875e1
+print(type(of: exponent_double))
+
+let onemillion = 1_000_000
+print(type(of: onemillion))
+*/
+
+//5. Integer Type Conversion
+/*
+ let three = 3
 let pointdecimals=0.14159
 let pi = Double(three) + pointdecimals
 print(pi)
 
 let integerPi = Int(pi)
 print(integerPi)
+ */
+
+
+
+//6. TYPE ALIASES
+/*typealias height = (ft: Int, inch: Int)
+var myht:height
+myht.ft = 5
+myht.inch = 9
+print(myht)
+print(type(of: myht))
+print(myht.ft)
+print(type(of: myht.ft))
+*/
+
+//7. Booleans
+/*
+ let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious{
+    print("Very tasty!")
+}
+
+else {
+    print("eww, turnips are disgusting")
+}
+print(type(of: orangesAreOrange))
+
+let i=1
+if i==1
+{ print("working fine")
+}
+*/
