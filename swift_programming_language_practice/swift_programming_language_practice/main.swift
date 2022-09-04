c94214f407ffe1b73b1ea3f1a451a80b0cc756f2
Aadil <aadiladheesh@gmail.com>
2022-09-04 18:22:48 +0530

    Committing examples of Dictionaries

--- swift_programming_language_practice/swift_programming_language_practice/main.swift
+++ swift_programming_language_practice/swift_programming_language_practice/main.swift
@@ -68,3 +68,81 @@ for items in shoppinglist{      //print all the elements in the array using for 
 for (idx,item) in shoppinglist.enumerated(){        //to print the item no using enumerated
     print("Item \(idx): \(item)")
 }
+
+
+
+//DICTIONARIES
+//1.
+var namesofintegers:[Int:String] = [:]
+print(namesofintegers)  //prints [:]
+
+namesofintegers[16] = "Sixteen"
+print(namesofintegers)      //prints [16: "Sixteen"]
+namesofintegers = [:]
+print(namesofintegers)    //prints [:]
+
+
+//Two ways of declaring it
+var airports:[String:String] = ["YYZ":"Torronto Pearson","DUB":"Dublin"]        //Full hand form
+print(airports)
+var Airports = ["YYZ":"Torronto Pearson","DUB":"Dublin"]        //Short hand form
+print(Airports)
+
+print("The airports dictionaries contain \(airports.count) items")
+
+if airports.isEmpty{
+    print("No elements in dictionary")
+}
+else{
+    print("There are elements in the dictionary")
+}
+
+airports["LHR"] = "London"      //Added this new key value pair to the dictionary
+print(airports)
+
+airports["LHR"] = "London Heathrow"        //Can also change the value of the element of a particular key
+print(airports)
+
+if let oldValueAirport = airports.updateValue("Dublin Airport", forKey: "DUB"){         //to update a value of the key
+    print("The old value of DUB is \(oldValueAirport)")
+}
+print(airports)
+
+if let airportsName = airports["DUB"]{
+    print("The name of the airport is \(airportsName)")
+}
+else{
+    print("There isnt a key value pair with that key")
+}
+
+airports["APL"] = "Apple International"     //introducing a fake entry
+print(airports)
+airports["APL"] = nil       //method to remove a key-value pair
+print(airports) //prints ["LHR": "London Heathrow", "DUB": "Dublin Airport", "YYZ": "Torronto Pearson"]
+
+if let removedValue = airports.removeValue(forKey: "DUB"){
+    print("The removed airport is \(removedValue)")
+}
+else{
+    print("There isnt a key value pair with that key")
+}
+print(airports)// prints ["LHR": "London Heathrow", "YYZ": "Torronto Pearson"]
+
+for (airportCode, airportName) in airports{
+    print("\(airportCode):\(airportName)")
+}
+//prints LHR:London Heathrow
+//YYZ:Torronto Pearson
+
+for airportCode in airports.keys{           //Access the all keys in the dictionary using the for loop
+    print("Airport code is \(airportCode)")
+}
+for airportName in airports.values{         //Access the all keys in the dictionary using the for loop
+    print("Airport name is\(airportName)")
+}
+
+let AirportCode = [String](airports.keys)       //Code to just take the keys and store them in the form of an array
+print(AirportCode)
+
+let AirportName = [String](airports.values)      //Code to just take the values and store them in the form of an array
+print(AirportName)
