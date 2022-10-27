c122888931b422cd2f32352187d271b080eab511
Aadil <aadiladheesh@gmail.com>
2022-10-27 10:47:43 +0530

    Stored Properties

--- swift_programming_language_practice/main.swift
+++ swift_programming_language_practice/main.swift
@@ -1,39 +1,35 @@
-//Accessing Properties
-print("The width of Resolution is \(someResolution.width)")
-print("the wiodth of someVideoMode is \(someVideoMode.resolution.width)")
-someVideoMode.resolution.width = 1280
-print("The width of someVideoMode now is \(someVideoMode.resolution.width)")
+//PROPERTIES
 
-//Structures and enumerations are value Types
-cinema.width = 2048
-print("cinema is now\(cinema.width) pixels wide")
-print("hd is still \(hd.width) pixels wide")
 
-enum CompassPoint{
-    case north, south, east, west
-    mutating func turnNorth() {
-        self = .north
-    }
+//Stored Properties
+
+struct FixedLenghthRange{
+    var firstValue: Int
+    var length: Int
 }
-var currentDirection = CompassPoint.west
-let rememberedDirection = currentDirection
-currentDirection.turnNorth()
+var rangeOfThreeValues = FixedLenghthRange(firstValue: 0, length: 3)
+rangeOfThreeValues.firstValue = 6
 
-print("The current direction is \(currentDirection)")   //The current direction is north
-print("The remembered directions is \(rememberedDirection)")    //The remembered directions is west
+let rangeofFourValues = FixedLenghthRange(firstValue: 0, length: 4)
+//rangeofFourValues.firstValue = 6    //Cannot assign to property: 'rangeofFourValues' is a 'let' constant
 
-//Classes are Reference Types
-let tenEighty = VideoMode()
-tenEighty.resolution = hd
-tenEighty.interlaced = true
-tenEighty.name = "1080i"
-tenEighty.frameRate = 25.0
+//Lazy Stored properties
 
-let alsoTenEighty = tenEighty   //30.0 framerate is referred and assigned to the tenEighty as well
-alsoTenEighty.frameRate = 30.0
-print("The framerate of tenEighty is \(tenEighty.frameRate)")
-
-//Identity Operators
-if tenEighty === alsoTenEighty{
-    print("tenEightty and alsoTenEighty refer to the same VideoMode instance")
+class DataImporter{
+    var fileName = "data.txt"
 }
+
+class DataManager{
+    lazy var Importer = DataImporter()
+    var Data : [String] = []
+}
+
+let manager = DataManager()
+manager.Data.append("Some data")
+manager.Data.append("Some more data")
+// data importer class was not called because the data manager handled the date by itself and importer was not called
+
+print(manager.Importer.fileName)    //prints data.txt
+//Here we are accesing the DataImporter class as we created the instance in by accessing the "Importer"
+
+
